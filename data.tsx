2bc428fcfafdf001e53f7b739d6c4052983b27eb
jiseonBaek <js.coding.place@gmail.com>
2021-10-17 18:55:09 +0900

    feat: fetch Data 생성

---
+++ data.tsx
@@ -0,0 +1,20 @@
+import { RiChatSmile2Line } from 'react-icons/ri';
+import { SiSpringCreators } from 'react-icons/si';
+import { IService } from './type';
+
+
+
+
+export const services: IService[] = [
+	{
+		title: '긍정적인 개발자',
+		about: '갑작스러운 일이 생겨도 항상 긍정적으로 생각해 일에 스트레스를 받지 않습니다.',
+		Icon: RiChatSmile2Line,
+	},
+	{
+		title: '유연한 개발자',
+		about: '',
+		Icon: SiSpringCreators,
+	},
+	
+]
---
+++ type.tsx
@@ -0,0 +1,7 @@
+import { IconType } from 'react-icons';
+
+export type IService {
+	title: string,
+	about: string,
+	Icon: IconType,
+}
