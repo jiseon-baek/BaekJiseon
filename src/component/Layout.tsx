f05e78714e8af58a65dbeb52ab7c8695d01edd39
jiseonBaek <js.coding.place@gmail.com>
2021-11-02 00:23:49 +0900

    chore: 문서 수정

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -5,16 +5,15 @@ import "tailwindcss/tailwind.css";
 
 type Props = {
 	children: any,
-	
+
 }
 
-const Layout: React.FC<Props> = ({children}) => {
+const Layout: React.FC<Props> = ({ children }) => {
 	return (
 		<>
-		
-		<div>
-			{children}
-		</div>
+			<div>
+				{children}
+			</div>
 		</>
 	)
 }
